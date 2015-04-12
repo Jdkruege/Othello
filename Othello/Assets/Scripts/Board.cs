6496abaa24820595ca92587683839f8fe0746ec0
Jdkruege <jdkuege@iastate.edu>
2015-04-11 21:21:39 -0500

    Base Board laid out
    The basis of the game has been laid out. Some interfaces have been
    created to facilitate playing the actual game. Current milestone is
    getting two players to work.

---
+++ Othello/Assets/Scripts/Board.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+
+    public GameObject[][] BoardPositions;
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Othello/Assets/Scripts/Tile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+    public GameObject piece;
+
+    public void OnMouseDown()
+    {
+        GameObject spawnedPiece = Instantiate(piece);
+
+        spawnedPiece.transform.position = this.transform.position;
+
+        spawnedPiece.transform.Translate(new Vector3(0, 2, 0));
+    }
+
+}
