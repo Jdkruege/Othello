7215e83ce37536c0f85b4566d60cf9d4e51e51d8
Jdkruege <jdkuege@iastate.edu>
2015-04-13 11:14:41 -0500

    Added a little more functionality
    Not much added here, but want to make sure my progress is uploaded.

--- Othello/Assets/Scripts/Board.cs
+++ Othello/Assets/Scripts/Board.cs
@@ -1,12 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Board : MonoBehaviour {
+public class Board {
 
-    public GameObject[][] BoardPositions;
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public pieces[,] board;
+    public enum pieces {white, black};
+
+    public void start()
+    {
+        ResetBoard();
+    }
+    
+    public void update()
+    {
+
+    }
+
+    public void AvailableMoves()
+    {
+
+    }
+
+    public void ResetBoard()
+    {
+        board = new pieces[8,8];
+
+        board[3, 3] = pieces.black;
+        board[3, 4] = pieces.white;
+        board[4, 3] = pieces.white;
+        board[4, 4] = pieces.black;
+    }
 }
